7fb4fc84eb3a4f5d9b9f288b21f7a021a905f2d2
Hoàng Việt Dũng <dung.hv194254@sis.hust.edu.vn>
2022-05-09 14:53:57 +0700

    init commit

---
+++ app/src/main/java/com/example/mobiledevhomework20212/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mobiledevhomework20212;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
