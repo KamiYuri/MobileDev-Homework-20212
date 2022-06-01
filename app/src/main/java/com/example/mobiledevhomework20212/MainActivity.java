3ead067dcad37a5520adc4e0c90a061fbce3c411
Hoàng Việt Dũng <dung.hv194254@sis.hust.edu.vn>
2022-06-01 17:22:37 +0700

    create layout

--- app/src/main/java/com/example/mobiledevhomework20212/MainActivity.java
+++ app/src/main/java/com/example/mobiledevhomework20212/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.mobiledevhomework20212;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
