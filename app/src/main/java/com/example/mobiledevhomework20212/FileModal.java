b7d2b7410ca9954fa9d78b10b92771998192356c
Hoàng Việt Dũng <dung.hv194254@sis.hust.edu.vn>
2022-07-13 10:22:22 +0700

    finish create, delete, rename file

---
+++ app/src/main/java/com/example/mobiledevhomework20212/FileModal.java
@@ -0,0 +1,89 @@
+package com.example.mobiledevhomework20212;
+
+import android.os.Build;
+import android.util.Pair;
+import android.webkit.MimeTypeMap;
+
+import androidx.annotation.RequiresApi;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+public class FileModal {
+
+    private static final Map<String, List<String>> fileType;
+    static {
+        fileType = new HashMap<>();
+        fileType.put("image", Arrays.asList("png", "jpg", "jpeg", "gif", "bmp"));
+        fileType.put("audio", Arrays.asList("mp3", "wav", "ogg", "midi", "m4a"));
+        fileType.put("video", Arrays.asList("mp4", "rmvb", "avi", "flv", "3gp"));
+        fileType.put("web", Arrays.asList("png", "jpg", "jpeg", "gif", "bmp"));
+        fileType.put("zip", Arrays.asList("jar", "zip", "rar", "gz"));
+        fileType.put("text", Arrays.asList("txt"));
+    }
+
+    private File file;
+
+    public FileModal(File file){
+        this.file = file;
+    }
+
+    public File getFile(){
+        return this.file;
+    }
+
+    public void setFile(File file){
+        this.file = file;
+    }
+
+    public String getAbsolutePath(){
+        return file.getAbsolutePath();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public String getExtension(){
+        Optional<String> optional = Optional.ofNullable(getName())
+                .filter(f -> f.contains("."))
+                .map(f -> f.substring(getName().lastIndexOf(".") + 1));
+        return optional.orElse("");
+    }
+
+    public String getName(){
+        return file.getName();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public String getType(){
+        for (Map.Entry<String, List<String>> entry : fileType.entrySet()){
+            if (Arrays.asList(entry.getValue().toArray()).contains(getExtension())){
+                return entry.getKey();
+            }
+        }
+        return "";
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public int getIconDrawable(){
+        if (getType().isEmpty()) {
+            return R.drawable.ic_baseline_folder_open_24;
+        }
+        else {
+            switch (getType()){
+                case "text":
+                    return R.drawable.ic_baseline_text_snippet_24;
+                case "image":
+                    return R.drawable.ic_baseline_image_24;
+                case "video":
+                    return R.drawable.ic_baseline_video_file_24;
+                case "audio":
+                    return R.drawable.ic_baseline_audio_file_24;
+                default:
+                    return R.drawable.ic_baseline_file_24;
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/mobiledevhomework20212/MainActivity.java
+++ app/src/main/java/com/example/mobiledevhomework20212/MainActivity.java
@@ -1,36 +1,45 @@
 package com.example.mobiledevhomework20212;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Settings;
 import android.util.Log;
-import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.MimeTypeMap;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.PopupMenu;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.Toolbar;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Dictionary;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,14 +47,12 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_CODE = 123;
 
-    Map<String, List<String>> fileType = new HashMap<>();
-
     private File dir;
     private String dirPath;
     private RecyclerView recyclerView;
     private TextView dirPathTextView;
 
-    private ArrayList<Pair<Integer, String>> fileInfo;
+    private ArrayList<FileModal> filesList;
     private CustomAdapter adapter;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
@@ -53,44 +60,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         requestPerms();
-        setFileTypeData();
 
         //Initialize ArrayLists
-        fileInfo = new ArrayList<>();
+        filesList = new ArrayList<>();
 
         dirPathTextView = findViewById(R.id.dirPathText);
-
         dirPath = Environment.getExternalStorageDirectory().getAbsolutePath();
 
         refreshListFiles();
 
         // Set adapter for listview
+        adapter = new CustomAdapter(filesList);
         recyclerView = findViewById(R.id.recyclerView);
-        adapter = new CustomAdapter(fileInfo);
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        registerForContextMenu(recyclerView);
 
         setDirPath();
     }
 
-    private void setFileTypeData() {
-        fileType.put("image", Arrays.asList("png", "jpg", "jpeg", "gif", "bmp"));
-        fileType.put("audio", Arrays.asList("mp3", "wav", "ogg", "midi"));
-        fileType.put("video", Arrays.asList("jsp", "html", "htm", "js", "php"));
-        fileType.put("web", Arrays.asList("png", "jpg", "jpeg", "gif", "bmp"));
-        fileType.put("zip", Arrays.asList("jar", "zip", "rar", "gz"));
-        fileType.put("text", Arrays.asList("txt"));
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.option_item_menu, menu);
+        return true;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    private String getFileType(String ext) {
-        for (Map.Entry<String, List<String>> entry : fileType.entrySet()){
-            if (Arrays.asList(entry.getValue().toArray()).contains(ext)){
-                return entry.getKey();
-            }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.addNewFolder){
+            addNewFolder();
         }
-        return "";
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void setDirPath() {
@@ -104,26 +110,16 @@ public class MainActivity extends AppCompatActivity {
             File[] files = dir.listFiles();
 
             //reset ArrayLists
-            fileInfo.clear();
+            filesList.clear();
+
+            if(!dir.getAbsolutePath().equals(Environment.getExternalStorageDirectory().getAbsolutePath())){
+                filesList.add(new FileModal(new File("/..")));
+            }
 
             assert files != null;
             for (File file : files) {
-                String ext = MimeTypeMap.getFileExtensionFromUrl(String.valueOf(file));
-                if (ext.isEmpty()) {
-                    fileInfo.add(new Pair<>(R.drawable.ic_baseline_folder_open_24, file.getName()));
-                }
-                else {
-                    switch (getFileType(ext)){
-                        case "text":
-                            fileInfo.add(new Pair<>(R.drawable.ic_baseline_text_snippet_24, file.getName()));
-                            break;
-                        case "image":
-                            fileInfo.add(new Pair<>(R.drawable.ic_baseline_image_24, file.getName()));
-                            break;
-                        default:
-                            fileInfo.add(new Pair<>(R.drawable.ic_baseline_file_24, file.getName()));
-                    }
-                }
+                Log.v("refreshListFiles", new FileModal(file).getExtension());
+                filesList.add(new FileModal(file));
             }
         } catch (Exception e) {
             String error = e + "\n\nMessage: " + e.getMessage();
@@ -134,10 +130,10 @@ public class MainActivity extends AppCompatActivity {
     private void requestPerms() {
         if (Build.VERSION.SDK_INT >= 23) {
             if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                Log.v("TAG", "Permission denied.");
+                Log.v("requestPerms", "Permission denied.");
                 requestPermissions(EXTERNAL_PERMS, REQUEST_CODE);
             } else {
-                Log.v("TAG", "Permission granted.");
+                Log.v("requestPerms", "Permission granted.");
             }
         }
 
@@ -148,15 +144,243 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    @SuppressLint("LongLogTag")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_CODE) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Log.v("TAG", "Permission granted.");
+                Log.v("onRequestPermissionsResult", "Permission granted.");
             } else {
-                Log.v("TAG", "Permission denied.");
+                Log.v("onRequestPermissionsResult", "Permission denied.");
             }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void fileOpen(FileModal file){
+        try{
+            String oldDirPath = dirPath;
+
+            dirPath = (new File(dirPath + "/" + file.getName())).getCanonicalPath();
+
+            Log.v("fileOpen", dirPath);
+
+            File f = new File(dirPath);
+            if (f.isDirectory()){
+                refreshListFiles();
+                refreshAdapter();
+                setDirPath();
+            }else{
+                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setAction(android.content.Intent.ACTION_VIEW);
+                //intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+
+                intent.setDataAndType(FileProvider.getUriForFile(this, this.getApplicationContext().getPackageName() + ".provider", f), MimeTypeMap.getSingleton().getMimeTypeFromExtension(new FileModal(f).getExtension()));
+                this.startActivity(intent);
+
+                dirPath = oldDirPath;
+            }
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    private void refreshAdapter() {
+        adapter.notifyDataSetChanged();
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        return super.onContextItemSelected(item);
+    }
+
+    public class CustomAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+        private ArrayList<FileModal> filesList;
+
+        public CustomAdapter(ArrayList<FileModal> filesList){
+            this.filesList = filesList;
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            View listViewItem = inflater.inflate(R.layout.listview_items, parent, false);
+
+            return new ViewHolder(listViewItem);
+        }
+
+        @SuppressLint("NonConstantResourceId")
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            FileModal file = filesList.get(position);
+
+            holder.itemView.setOnClickListener(v -> {
+                fileOpen(file);
+            });
+
+            ((ViewHolder)holder).fileIcon.setImageResource(file.getIconDrawable());
+            ((ViewHolder)holder).fileName.setText(file.getName());
+            ((ViewHolder)holder).optionButton.setOnClickListener(v -> {
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this, v);
+                popupMenu.getMenuInflater().inflate(R.menu.file_item_menu, popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(item -> {
+                    switch (item.getItemId()){
+                        case R.id.menu_item_select:
+                            select(file);
+                            return true;
+                        case R.id.menu_item_move:
+                            move(file);
+                            return true;
+                        case R.id.menu_item_copy:
+                            copy(file);
+                            return true;
+                        case R.id.menu_item_rename:
+                            rename(file);
+                            return true;
+                        case R.id.menu_item_delete:
+                            delete(file);
+                            return true;
+                        default:
+                            return false;
+                    }
+
+                });
+                popupMenu.show();
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return filesList.size();
+        }
+
+        private class ViewHolder extends RecyclerView.ViewHolder {
+            public TextView fileName;
+            public ImageView fileIcon;
+            public ImageButton optionButton;
+
+            public ViewHolder(@NonNull View itemView) {
+                super(itemView);
+
+                itemView.setSelected(true);
+
+                this.fileIcon = itemView.findViewById(R.id.fileIcon);
+                this.fileName = itemView.findViewById(R.id.fileName);
+                this.optionButton = itemView.findViewById(R.id.optionButton);
+            }
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void addNewFolder() {
+        try{
+
+            //NewFolder Dialog Builder
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+            LayoutInflater inflater = this.getLayoutInflater();
+            final View dialogView = inflater.inflate(R.layout.modal, null);
+            dialogBuilder.setView(dialogView);
+
+            final EditText txtNewFolder = dialogView.findViewById(R.id.modalText);
+
+            dialogBuilder.setTitle("New Folder");
+            dialogBuilder.setMessage("Enter name of new folder");
+            dialogBuilder.setPositiveButton("Done", (dialog, whichButton) -> {
+                String sNewFolderName = txtNewFolder.getText().toString();
+                File fNewFolder = new File(dirPath + "/" + sNewFolderName);
+                // create
+                if (fNewFolder.exists()){
+                    Toast.makeText(this, "File is exist", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                fNewFolder.mkdir();
+                //Refresh ListView
+                refreshListFiles();
+                refreshAdapter();
+            });
+            dialogBuilder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    //pass
+                }
+            });
+            AlertDialog b = dialogBuilder.create();
+            b.show();
+        }catch (Exception e){
+            Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void rename(FileModal file) {
+        try{
+            //RenameFolder Dialog Builder
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+            LayoutInflater inflater = this.getLayoutInflater();
+            final View dialogView = inflater.inflate(R.layout.modal, null);
+            dialogBuilder.setView(dialogView);
+
+            final EditText modalText = dialogView.findViewById(R.id.modalText);
+            modalText.setText(file.getName());
+
+            dialogBuilder.setTitle("Rename Folder");
+            dialogBuilder.setMessage("Enter new name of folder");
+            dialogBuilder.setPositiveButton("Done", (dialog, whichButton) -> {
+                File f = file.getFile();
+                File fRename = new File(dirPath+ "/" + modalText.getText().toString());
+
+                if (fRename.exists()){
+                    Toast.makeText(this, "File is exist", Toast.LENGTH_LONG).show();
+                    return;
+                }
+                f.renameTo(fRename);
+
+                //Refresh ListView
+                refreshListFiles();
+                refreshAdapter();
+            });
+            dialogBuilder.setNegativeButton("Cancel", (dialog, whichButton) -> {
+                //pass
+            });
+            AlertDialog alertDialog = dialogBuilder.create();
+            alertDialog.show();
+        }catch (Exception e){
+            Log.v("TAG", e.toString());
+        }
+    }
+
+    private void copy(FileModal file) {
+    }
+
+    private void move(FileModal file) {
+    }
+
+    private void select(FileModal file) {
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private void delete(FileModal file) {
+        List<String> command = new ArrayList<>();
+
+        try {
+            command.add("/system/bin/rm");
+            command.add("-rf");
+            command.add(file.getAbsolutePath());
+
+            // start the subprocess
+            ProcessBuilder pb = new ProcessBuilder(command);
+            Process process = pb.start();
+            process.waitFor();
+
+            //Refresh ListView
+            refreshListFiles();
+            refreshAdapter();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
